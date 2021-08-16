e0620dc696ef45f37b292b6f8fdce9ac9b926ee8
mygizli04 <42012824+mygizli04@users.noreply.github.com>
2021-08-16 18:46:04 +0400

    Add docs to the API

--- index.ts
+++ index.ts
@@ -1,33 +1,63 @@
-import fetch from "node-fetch";
+import fetch from "node-fetch"; // Used for contacting the API
 
-const apiUrl = "https://api.breezecodes.com";
+const apiUrl = "https://api.breezecodes.com"; // The API's base URL
+
+/**
+ * Possible values a username/pass check can return
+ */
 type CheckResponse = "Account" | "Password" | "True" | "KeyError";
 
-export async function check (key: string, username: string, password: string): Promise<void> {
+/**
+ * The results of a username/password check.
+ */
+type CheckResult = "InvalidAccount" | "WrongPassword" | null;
+
+/**
+ * Check if a username/password combination is correct.
+ * 
+ * @async
+ * @param  {string} key The API key to be used. Details available on {@link https://api.breezecodes.com/ API page}
+ * @param  {string} username Username to be checked.
+ * @param  {string} password Password to be checked.
+ * @returns [boolean, {@link CheckResult}]
+ */
+export async function check (key: string, username: string, password: string): Promise<[boolean, CheckResult]> {
     return new Promise(async (resolve, reject) => {
-        let response = await (await fetch(apiUrl + "/checkaccount/" + key + "/" + username + "/" + password)).text() as CheckResponse
+        let response = await (await fetch(apiUrl + "/checkaccount/" + key + "/" + username + "/" + password)).text() as CheckResponse;
 
         switch (response) {
             case "Account":
-                reject("No such account exists.");
+                resolve([false, "InvalidAccount"]);
                 break;
             case "KeyError":
                 reject("Invalid API key.");
             case "Password":
-                reject("Invalid password.");
+                resolve([false, "WrongPassword"]);
                 break;
             case "True":
-                resolve();
+                resolve([true, null]);
         }
 
     });
 };
 
-type CreateResponse = "Username" | "True" | "KeyError"
+/**
+ * Possible responses of a create call.
+ */
+type CreateResponse = "Username" | "True" | "KeyError";
 
+// On errors we just reject the promise, so no "CreateResult" or anything.
+
+/**
+ * @async
+ * @param  {string} key The API key to be used. Details available on {@link https://api.breezecodes.com/ API page}.
+ * @param  {string} username The username to create.
+ * @param  {string} password The password to create.
+ * @returns void
+ */
 export async function create (key: string, username: string, password: string): Promise<void> {
     return new Promise(async (resolve, reject) => {
-        let response = await (await fetch(apiUrl + "/createaccount/" + key + "/" + username + "/" + password)).text() as CreateResponse
+        let response = await (await fetch(apiUrl + "/createaccount/" + key + "/" + username + "/" + password)).text() as CreateResponse;
 
         switch (response) {
             case "Username":
